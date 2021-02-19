be80ee4c5ac8d871e2eea8d0381619f4d8ae7371
Crunkz <74662096+SrCafezito@users.noreply.github.com>
2021-02-19 12:28:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+	const ping = new Date();
+	ping.setHours(ping.getHours() - 3);
+	console.log(
+		`Ping recebido às ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`
+	);
+	response.sendStatus(200);
+});
+app.listen(process.env.PORT); // Recebe solicitações que o deixa online
+
+const Discord = require('discord.js'); //Conexão com a livraria Discord.js
+const client = new Discord.Client(); //Criação de um novo Client
+const config = require('./config.json'); //Pegando o prefixo do bot para respostas de comandos
+
+client.on('message', message => {
+	if (message.author.bot) return;
+	if (message.channel.type == 'dm') return;
+	if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase()))
+		return;
+	if (
+		message.content.startsWith(`<@!${client.user.id}>`) ||
+		message.content.startsWith(`<@${client.user.id}>`)
+	)
+		return;
+
+	const args = message.content
+		.trim()
+		.slice(config.prefix.length)
+		.split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	try {
+		const commandFile = require(`./commands/⚙️ Moderação/${command}.js`);
+		commandFile.run(client, message, args);
+	} catch (err) {
+		console.error('Erro:' + err);
+	}
+});
+
+client.on('message', message => {
+     if (message.author.bot) return;
+     if (message.channel.type == 'dm') return;
+     if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
+     if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+
+    const args = message.content
+        .trim().slice(config.prefix.length)
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+        const commandFile = require(`./commands/⛵ Br/${command}.js`);
+        commandFile.run(client, message, args);
+    } catch (err) {
+    console.error('Erro:' + err);
+  }
+});
+
+client.on('message', message => {
+     if (message.author.bot) return;
+     if (message.channel.type == 'dm') return;
+     if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase())) return;
+     if (message.content.startsWith(`<@!${client.user.id}>`) || message.content.startsWith(`<@${client.user.id}>`)) return;
+
+    const args = message.content
+        .trim().slice(config.prefix.length)
+        .split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    try {
+        const commandFile = require(`./commands/😆 Fun/${command}.js`);
+        commandFile.run(client, message, args);
+    } catch (err) {
+    console.error('Erro:' + err);
+  }
+});
+
+client.on('message', message => {
+	if (message.author.bot) return;
+	if (message.channel.type == 'dm') return;
+	if (!message.content.toLowerCase().startsWith(config.prefix.toLowerCase()))
+		return;
+	if (
+		message.content.startsWith(`<@!${client.user.id}>`) ||
+		message.content.startsWith(`<@${client.user.id}>`)
+	)
+		return;
+
+	const args = message.content
+		.trim()
+		.slice(config.prefix.length)
+		.split(/ +/g);
+	const command = args.shift().toLowerCase();
+
+	try {
+		const commandFile = require(`./commands/⚔️ Útil/${command}.js`);
+		commandFile.run(client, message, args);
+	} catch (err) {
+		console.error('Erro:' + err);
+	}
+});
+
+client.on("ready", () => {
+  let activities = [
+    `O meu prefixo é c!`,
+    `Utilize c!help`,
+    `❤️ Assistindo animes românticos`,
+    `🎙️ Ouvindo podcast`,
+    `Jogando 🎮 Minecraft`,
+    `Meu personagem favorito é o 🕵️‍♂️ Sherlock Holmes`,
+    `Eu tenho um 😍 crush!`,
+    `${client.users.cache.size} usuários`
+    ],
+    i = 0;
+  setInterval( () => client.user.setActivity(`${activities[i++ % activities.length]}`, {
+        type: "STREAMING",
+        url:"https://www.twitch.tv/ckzbrokstf"
+      }), 15000);
+  client.user
+      .setStatus("online")
+      .catch(console.error);
+console.log(`
++--------------+
+|  BOT ONLINE  |
++--------------+
+`);
+});
